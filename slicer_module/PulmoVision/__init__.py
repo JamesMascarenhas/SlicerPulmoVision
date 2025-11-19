c8d189f4f2614f9d614b16a1e92fd499abcd96c9
James Mascarenhas <james.mas27@gmail.com>
2025-11-18 20:02:53 -0500

    PulmoVision __init__ module updated

--- slicer_module/PulmoVision/__init__.py
+++ slicer_module/PulmoVision/__init__.py
@@ -1,1 +1,9 @@
+# __init__.py inside slicer_module/PulmoVision
+# Expose the scripted module classes from PulmoVision.py
 
+from .PulmoVision import (
+    PulmoVision,
+    PulmoVisionWidget,
+    PulmoVisionLogic,
+    PulmoVisionTest,
+)
